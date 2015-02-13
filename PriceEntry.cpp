58c0bcfd4f444082655ff678f526e09f842a5eb8
xiihaa <xiihaa0002@gmail.com>
2015-02-13 15:30:33 -0500

    sthnew

--- PriceEntry.cpp
+++ PriceEntry.cpp
@@ -7,3 +7,13 @@ ostream & operator << (std::ostream & os, const Tick & output)
 	os << "Date: " << output.date << ", Time: " << output.time << ", Price: " << output.price << ", Volume: " << output.volume << endl;
 	return os;
 }
+
+int convertDate(const string & datetime){  //take the string of the format 20140804:10:00:00.574914, to convert to integer 20140804
+	int theyear;
+	int themonth;
+	int theday;
+	int theYear = stoi(datetime.substr(0, 4));
+	int theMonth = stoi(datetime.substr(4, 2));
+	int theDay = stoi(datetime.substr(6, 2));
+	return theYear * 10000 + theMonth * 100 + theDay;
+}
