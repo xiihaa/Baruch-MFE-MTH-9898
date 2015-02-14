f683fbd7402142549e889cca5b89bb10bee29c54
xiihaa <xiihaa0002@gmail.com>
2015-02-14 00:00:07 -0500

    user

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int main(){
-	ifstream in("/home/xia/9898/Baruch-MFE-MTH-9898/data10k.csv");
+	ifstream in("C:\\Users\\xii\\Dropbox\\MFE\\9898\\hw1\\hw1\\data10k.csv");
 	Scrub newseries;
 	string s = "20140804:10:00:00.574914, 1173.56, 471577";
 	cout << 1 << endl;
@@ -18,8 +18,11 @@ int main(){
 	cout << 3 << endl;
 	cout << newseries[0] << endl;
 	cout << newseries[1] << endl;
-	cout << comparetickBytime(newseries[0], newseries[1]) << endl;
-	cout << comparetickByprice(newseries[0], newseries[1]) << endl;
-	cout << comparetickByvolumn(newseries[0], newseries[1]) << endl;
-	return 0;
+	newseries.sortByTime();
+	cout << 4 << endl;
+	cout << newseries[0] << endl;
+	cout << newseries[1] << endl;
+	for (int i = 0; i < 20; i++){
+		cout << newseries[i] << endl;
+	}
 }
